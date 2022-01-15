94a7ec38d66ea3a22f8b25d303bb2800d1110670
Aron Bijl <aron.bijl@me.com>
2022-01-15 17:31:34 +0100

    Bug Fix Search

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -18,6 +18,7 @@ productRouter.get(
       req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
     const max =
       req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+      console.log(req.query);
     const rating =
       req.query.rating && Number(req.query.rating) !== 0
         ? Number(req.query.rating)
@@ -27,6 +28,7 @@ productRouter.get(
     const sellerFilter = seller ? { seller } : {};
     const categoryFilter = category ? { category } : {};
     const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
+    console.log(priceFilter);
     const ratingFilter = rating ? { rating: { $gte: rating } } : {};
     const sortOrder =
       order === 'lowest'
